736ce065a129bcdbd740817e601361db09c4e1b9
ShaozhuDianxia <lavieestbellejetaime@gmail.com>
2014-04-26 23:38:47 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -10,7 +10,7 @@
 ## 1.set the value of the matrix;
 ## 2.get the value of the matrix;
 ## 3.set the value of the inverse;
-## 4.get the value of the mean.
+## 4.get the value of the inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
